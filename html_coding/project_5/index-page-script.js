386065b614d97764ca7b9fd9c89b2609126b34c8
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2025-10-12 02:29:49 +0300

    project_5

---
+++ html_coding/project_5/index-page-script.js
@@ -0,0 +1,5 @@
+//Кнопка перехода в бургер-меню, раздел Links
+const linksBtn = document.querySelector('.heroLinksBtn');
+linksBtn.addEventListener('click', function() {
+    window.location.href = 'second-page.html';
+});
---
+++ html_coding/project_5/second-page-script.js
@@ -0,0 +1,5 @@
+//Кнопка возврата из бургер-меню
+const backBtn = document.querySelector('.back');
+backBtn.addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
