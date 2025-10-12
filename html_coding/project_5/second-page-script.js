af1ba7e531ea563f6132d9ece91f6fa39517aa6f
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2025-10-12 08:58:50 +0300

    project_5

--- html_coding/project_5/second-page-script.js
+++ html_coding/project_5/second-page-script.js
@@ -3,3 +3,13 @@ const backBtn = document.querySelector('.back');
 backBtn.addEventListener('click', function() {
     window.location.href = 'index.html';
 });
+
+
+const darkthemeBtn = document.querySelector('.darkthemeBtn');
+const bodyElement = document.body;
+if (darkthemeBtn) {
+   darkthemeBtn.addEventListener('click', function() {
+    bodyElement.classList.toggle('dark-theme');
+    console.log('Выбрана темная тема');
+}); 
+}
