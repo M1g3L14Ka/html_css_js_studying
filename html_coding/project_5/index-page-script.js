acc92824fe19e4b29b473020a1ce6afe07e2ed0d
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2025-10-12 08:53:39 +0300

    Delete html_coding/project_5 directory

--- html_coding/project_5/index-page-script.js
+++
@@ -1,5 +0,0 @@
-//Кнопка перехода в бургер-меню, раздел Links
-const linksBtn = document.querySelector('.heroLinksBtn');
-linksBtn.addEventListener('click', function() {
-    window.location.href = 'second-page.html';
-});
--- html_coding/project_5/second-page-script.js
+++
@@ -1,5 +0,0 @@
-//Кнопка возврата из бургер-меню
-const backBtn = document.querySelector('.back');
-backBtn.addEventListener('click', function() {
-    window.location.href = 'index.html';
-});
