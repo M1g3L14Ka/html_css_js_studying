c55b8181cedf89366292325091400975b373ac67
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2025-10-12 08:56:42 +0300

    project_5

--- html_coding/project_5/index-page-script.js
+++ html_coding/project_5/index-page-script.js
@@ -1,4 +1,4 @@
-//Кнопка перехода в Links
+//Кнопка перехода в бургер-меню, раздел Links
 const linksBtn = document.querySelector('.heroLinksBtn');
 linksBtn.addEventListener('click', function() {
     window.location.href = 'second-page.html';
