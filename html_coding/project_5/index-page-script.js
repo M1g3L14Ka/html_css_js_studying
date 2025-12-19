8669a95505b6eef4d7c7359bc7a95c2d72192b13
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2025-12-19 22:19:57 +0300

    19.12.25

--- html_coding/project_5/index-page-script.js
+++
@@ -1,5 +0,0 @@
-//Кнопка перехода в Links
-const linksBtn = document.querySelector('.heroLinksBtn');
-linksBtn.addEventListener('click', function() {
-    window.location.href = 'second-page.html';
-});
